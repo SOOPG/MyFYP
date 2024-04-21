213be31626735d62a4b204142e8550fbc04c48a9
SOOPG <wilem.mhsoo@gmail.com>
2024-04-22 04:24:45 +0800

    V0.0.2B
    -added hover effects to clickable area in home scene

--- scripts/sceneHomeHandler.gd
+++ scripts/sceneHomeHandler.gd
@@ -1,11 +1,44 @@
 extends CanvasLayer
 
+@onready var bed_control = $BedControl
+@onready var bed_hover_icon = $BedControl/bed_hover
+@onready var study_control = $StudyControl
+@onready var study_hover_icon = $StudyControl/study_hover
+@onready var door_control = $DoorControl
+@onready var door_hover_icon = $DoorControl/door_hover
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Hover sprites are not visible at the start
+	bed_hover_icon.visible = false
+	study_hover_icon.visible = false
+	door_hover_icon.visible = false
+
+	# Connect the mouse_entered and mouse_exited signals
+	bed_control.mouse_entered.connect(self._on_bed_mouse_entered)
+	bed_control.mouse_exited.connect(self._on_bed_mouse_exited)
+	
+	study_control.mouse_entered.connect(self._on_study_mouse_entered)
+	study_control.mouse_exited.connect(self._on_study_mouse_exited)
+
+	door_control.mouse_entered.connect(self._on_door_mouse_entered)
+	door_control.mouse_exited.connect(self._on_door_mouse_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_bed_mouse_entered():
+	bed_hover_icon.visible = true
+
+func _on_bed_mouse_exited():
+	bed_hover_icon.visible = false
+
+func _on_study_mouse_entered():
+	study_hover_icon.visible = true
+
+func _on_study_mouse_exited():
+	study_hover_icon.visible = false
+
+func _on_door_mouse_entered():
+	door_hover_icon.visible = true
+
+func _on_door_mouse_exited():
+	door_hover_icon.visible = false
