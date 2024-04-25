3f06d4cc5e249504427d71ae1a1d674be4c8b542
SOOPG <wilem.mhsoo@gmail.com>
2024-04-25 08:25:22 +0800

    V0.7
    Fixed a major bug whereby in study minigame, the player will get stuck in a dead state

--- scripts/DistractionHandler.gd
+++ scripts/DistractionHandler.gd
@@ -61,12 +61,19 @@ func maybe_start_distraction():
 		active_distractions += 1  # increase the count of active distractions
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		clear_distraction()  # Player clicked the distraction, clear it
+	# If no distraction sprite, do nothing
+	if sprite.texture != distraction_texture:
+		return
+	# If there is a distraction sprite and the player clicked on the one, remove the distraction
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if sprite.texture == distraction_texture:
+			clear_distraction()
 
 func _on_distraction_timer_timeout():
 	# Time to trigger a distraction
 	sprite.texture = distraction_texture
+	# Enable Area2D input detection
+	set_process_input(true)
 	# Signal the parent or main handler if necessary
 	emit_signal("distraction_started", self.name)
 
@@ -76,6 +83,8 @@ func _on_cooldown_timer_timeout():
 
 func clear_distraction():
 	sprite.texture = idle_texture
+	# Disable Area2D input detection
+	set_process_input(false)
 	cooldown_timer.start(randf_range(min_cooldown_time, max_cooldown_time))
 	emit_signal("distraction_cleared", self.name)
 	active_distractions -= 1
